d8a02fe627288b99d42a067c7e7196de261d93eb
Marti <martidani1102@gmail.com>
2024-03-06 17:24:08 +0100

    mylib

--- IDS/src/it/unibs/ids/progetto/Main.java
+++ IDS/src/it/unibs/ids/progetto/Main.java
@@ -1,8 +1,15 @@
 package it.unibs.ids.progetto;
 
+import it.unibs.fp.mylib.Metodi;
+
 public class Main {
 
 	public static void main(String[] args) {
-				System.out.println("funziona");
+				
+		
+		
+	
+	
+	
 	}
 }
