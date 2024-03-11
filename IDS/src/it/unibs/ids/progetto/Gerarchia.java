e1260e12a7da5df2afdc5b52b756b525fa7f2ff5
Marti <martidani1102@gmail.com>
2024-03-11 15:47:03 +0100

    fix

--- IDS/src/it/unibs/ids/progetto/Gerarchia.java
+++ IDS/src/it/unibs/ids/progetto/Gerarchia.java
@@ -4,7 +4,27 @@ public class Gerarchia {
 	
 	/**
 	 * 
+	 * Attributi 
 	 * 
+	 * ArrarList <Nodo>
+	 *
+	 */
+	
+	
+	/**
+	 * 
+	 * Metodi
+	 * 
+	 * bool VerificafattoreConversione
+	 * void addFattoreConversione
+	 * void addInverso
+	 * void addTronsitivoFattoreConversione
+	 * 
+	 * 
+	 * void aggiungiFattoriconversione (nodo1, nodo2, double) insieme degli addFattori
+	 * 
+	 * 
+	 * VisualizzaFattoriConversione della foglia assegnata
 	 * 
 	 */
 
