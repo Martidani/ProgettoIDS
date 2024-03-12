715e6377f1aa21167e927d75e9b4f9257925e522
Marti <martidani1102@gmail.com>
2024-03-12 11:12:58 +0100

    Struttura albero

--- IDS/src/it/unibs/ids/progetto/Gerarchia.java
+++ IDS/src/it/unibs/ids/progetto/Gerarchia.java
@@ -1,15 +1,91 @@
 package it.unibs.ids.progetto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gerarchia {
 	
+	private static final double MIN_FATTORECONVERSIONE = 0.5;
+	private static final double MAX_FATTORECONVERSIONE = 2.0;
 	/**
 	 * 
 	 * Attributi 
-	 * 
-	 * ArrarList <Nodo>
-	 *
 	 */
 	
+	private ArrayList<Nodo> alberi;
+	private ArrayList<Nodo> foglie;
+	//private ArrayList<Nodo>
+
+	public Gerarchia() {
+		this.alberi = new ArrayList<>();
+		
+	}
+	
+	
+
+	public ArrayList<Nodo> getAlberi() {
+		return alberi;
+	}
+
+	public void addAlberi(Nodo albero) throws Exception {
+		if (albero.isRoot()) throw new Exception("Il nodo immesso non è radice");
+		alberi.add(albero);
+		addFoglie(albero);
+	}
+	
+	private void addFoglie(Nodo albero) {
+		
+		if (albero.getChildren() == null) 
+			foglie.add(albero);
+		else {
+			for (Nodo child : albero.getChildren()) {
+				addFoglie(child);
+			}
+		}
+	}
+	
+	
+	public void aggiungiFattoreConversione(Nodo nodo1, Nodo nodo2, double fattore) throws Exception {
+		if (!verificaFattoreConversione(fattore)) throw new 
+		Exception ("Fattore di conversione sbagliato, deve essere 0.5<__<2.0 ");
+		
+		addFattoreConversione(nodo1, nodo2, fattore);
+		addInversoFattoreConversione(nodo1, nodo2, fattore);
+		addTransitivoFattoreConversione(nodo1, nodo2, fattore);
+		
+	}
+	
+	public boolean verificaFattoreConversione(double fattore) {
+		if (fattore >= MAX_FATTORECONVERSIONE || fattore <= MIN_FATTORECONVERSIONE) 
+			return false;
+		
+		return true;
+	}
+	
+	public void addFattoreConversione(Nodo nodo1, Nodo nodo2, Double fattore) {
+		nodo1.addFattori(nodo2,fattore);
+	}
+	
+	public void addInversoFattoreConversione(Nodo nodo1, Nodo nodo2, double fattore) {
+		addFattoreConversione(nodo2, nodo1, 1/fattore);
+	}
+	
+	public void addTransitivoFattoreConversione(Nodo nodo1, Nodo nodo2, double fattore) {
+		
+		for (Nodo nodo3 : this.foglie) {
+			if (!nodo1.esisteFoglia(nodo3)) {
+				nodo1.addFattori(nodo3, calcTransitivo(nodo1,nodo2,nodo3));
+			}
+			
+		}
+		
+	}
+	
+	private Double calcTransitivo(Nodo nodo1,Nodo nodo2, Nodo nodo3) {
+		
+		
+		return 0.0;
+	}
 	
 	/**
 	 * 
--- IDS/src/it/unibs/ids/progetto/Nodo.java
+++ IDS/src/it/unibs/ids/progetto/Nodo.java
@@ -1,5 +1,108 @@
 package it.unibs.ids.progetto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
 public class Nodo {
+	
+	private String nome;
+	private boolean isLeaf;
+	private boolean isRoot;
+	private List<Nodo> children;
+	private HashMap<Nodo, Double> fattori;
+	private String campo;
+	private List<String[]> dominio;
+	
+	/**
+	 * COSTRUTTORE NON FOGLIA
+	 * @param nome
+	 * @param isRoot
+	 * @param campo
+	 * @param dominio
+	 */
+	
+	public Nodo(String nome,boolean isRoot, String campo, List<String[]> dominio) {
+	
+		this.isRoot = isRoot;
+		this.campo = campo;
+		this.dominio = dominio;
+		this.nome = nome;
+		this.isLeaf = false;
+		this.children = new ArrayList<>();
+	}
+
+	/**
+	 * COSTRUTTORE FOGLIA
+	 * @param nome
+	 * @param isRoot
+	 */
+	public Nodo(String nome, boolean isRoot) {
+		
+		this.nome = nome;
+		this.isRoot = isRoot;
+		this.isLeaf = true;
+		this.fattori = new HashMap<>();
+	}
+
+
+	/**
+	 * GETTER E SETTERS
+	 * 
+	 */
+
+	public String getNome() { return nome; }
+	public void setNome(String nome) { 	this.nome = nome; }
+
+	public boolean isLeaf() { return isLeaf;}
+	public void setLeaf(boolean isLeaf) { this.isLeaf = isLeaf;}
+
+	public boolean isRoot() {return isRoot;}
+	public void setRoot(boolean isRoot) {this.isRoot = isRoot;}
+
+	public List<Nodo> getChildren() {return children;}
+	public void setChildren(List<Nodo> children) {this.children = children;}
+
+	public HashMap<Nodo, Double> getFattori() { return fattori;}
+	public void addFattori(Nodo foglia, Double fattore) {
+		fattori.put(foglia,fattore);
+	}
+
+	public String getCampo() {return campo;}
+	public void setCampo(String campo) {this.campo = campo;}
+
+	public List<String[]> getDominio() {return dominio;}
+
+	public void setDominio(List<String[]> dominio) {this.dominio = dominio;}
+	
+	
+	// Metodi
+	
+	public void addChild(Nodo child) throws Exception {
+		if (this.isLeaf) throw new Exception("Le foglie non possono avere figli");
+		this.children.add(child);
+	}
+	
+	public boolean esisteFoglia(Nodo foglia) {
+		
+		if(this.getFattori().get(foglia) != null)
+			return true;
+		else
+			return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
